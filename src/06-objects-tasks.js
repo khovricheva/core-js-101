2b9b931975dd837c0827404f271484745b915f9b
khovricheva <kseniyap1102@gmail.com>
2020-12-14 23:16:04 +0200

    fix: formatting

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -19,13 +19,16 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(width, height) {
-  this.width = width;
-  this.height = height;
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
 }
-Rectangle.prototype.getArea = function () {
-  return this.width * this.height;
-};
 
 /**
  * Returns the JSON representation of specified object
